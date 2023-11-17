307ac4a2e931cc96954de6b170d66ffb4ce14f20
LucasVega2004 <luquitasvegablanco@gmail.com>
2023-11-17 19:03:36 +0000

    models+api

---
+++ database/migrations/2023_11_17_184941_create_modulos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('materia');
+            $table->integer('h_semanales');
+            $table->integer('h_totales');
+            $table->enum('turno', ['mañana', 'tarde']);
+            $table->string('aula');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modulos');
+    }
+};
